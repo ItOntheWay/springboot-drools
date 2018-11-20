f3011f3a7d66fd066a1f2c66ee0caf439e4c8015
jingpb <jpw_980319116@163.com>
2018-11-20 21:06:30 +0800

    first commit

---
+++ src/main/java/com/yuncong/drools/SpringbootDroolsApplication.java
@@ -0,0 +1,12 @@
+package com.yuncong.drools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDroolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDroolsApplication.class, args);
+	}
+}
